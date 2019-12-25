71c2cb65ed41aab373cce1e63152b01471a426df
caidingwen <caidingwen@lvmama.com>
2019-12-25 10:19:33 +0800

    update

---
+++ littiebee-stu/src/main/java/tech/dwen/stu/sword/TreeNode.java
@@ -0,0 +1,36 @@
+package tech.dwen.stu.sword;
+
+public class TreeNode {
+
+    private Integer value;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(Integer value){
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+}
