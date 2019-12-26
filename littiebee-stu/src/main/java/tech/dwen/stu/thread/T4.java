694607c809300f610e8ed4a198c5b6a93ae75149
caidingwen <caidingwen@lvmama.com>
2019-12-26 14:39:38 +0800

    Thread 的 join 方法

---
+++ littiebee-stu/src/main/java/tech/dwen/stu/thread/T4.java
@@ -0,0 +1,43 @@
+package tech.dwen.stu.thread;
+
+/**
+ * Thread 的 join 方法
+ * t.join(); 当前线程等待 t 线程执行完成
+ */
+public class T4 {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + "-开始");
+        Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "-开始");
+            for (int i = 0; i < 5; i++) {
+                try {
+                    Thread.sleep(1000L);
+                    System.out.println(Thread.currentThread().getName() + "-" +i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + "-结束");
+        },"T1");
+
+        Thread t2 = new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "-开始");
+            try {
+                // t2 线程等待 t1 线程执行完成
+                t1.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "-结束");
+        },"T2");
+
+        t1.start();
+        t2.start();
+
+        // main 线程等待 t2 线程执行完成
+        t2.join();
+        System.out.println(Thread.currentThread().getName() + "-结束");
+    }
+
+}
